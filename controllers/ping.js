c8e29758287238d2b4c4b472ce5da026e8440ab4
danillo7789 <danielbolajoko30@gmail.com>
2025-12-02 13:25:25 +0100

    stopped using ping.json and created an object in the ping controller

--- controllers/ping.js
+++ controllers/ping.js
@@ -1,7 +1,12 @@
 // controller.js
-const path = require('path');
+// const path = require('path');
 
-const data = require(path.join(__dirname, '../ping.json'));
+// const data = require(path.join(__dirname, '../ping.json'));
+
+const data = {
+  message: "This is a ping",
+  day: new Date().toDateString()
+}
 
 exports.getPing = (req, res) => {
   res.json(data);
